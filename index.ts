f2bd3a02bdcefad7b0ea0b49f67d4be0de79462f
Dizzy <itsdizzy40@gmail.com>
2016-09-06 22:34:18 +0200

    Added all needed classes to the main export file

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
 export { AuthModule } from './auth.module';
+export { IAuthConfig, AuthConfig } from './auth.config';
 export { AuthHttp } from './auth.http';
+export { TokenService } from './token.service';
+export { Token } from './token.model';
